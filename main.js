797ce33ed26cc9d7e271ecab310e2e3ff6bef501
Maharshi Patel <88446251+maharshi878@users.noreply.github.com>
2022-07-17 09:59:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function startClassification()
 {
   navigator.mediaDevices.getUserMedia({ audio: true});
-  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', modelReady);
+  classifier = ml5.soundClassifier('', modelReady);
 }
 
 function modelReady(){
